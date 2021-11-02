9391701078066e8ad6f944a66a7f4e20725e370a
Ankita <89608003+ankita92084@users.noreply.github.com>
2021-11-02 18:51:11 +0530

    Add files via upload

---
+++ emptylist.py
@@ -0,0 +1,31 @@
+
+lists=[[],4,6,[],9,8,16,6,8,9,1]
+i=0
+empty=[]
+while i <len(lists):
+    if lists[i]!=[]:
+        empty.append(lists[i])
+    i+=1
+print(empty)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
